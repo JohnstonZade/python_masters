03994bc9146f7ab7bd246acb55d7109de178b6f5
JohnstonZade <johza721@student.otago.ac.nz>
2021-06-17 18:08:35 +1200

    submit template

---
+++ submit_template.py
@@ -0,0 +1,24 @@
+computer = 'local'
+# computer = 'nesi'
+
+if computer == 'local':
+    python_masters_path = '~/python_masters/'
+else:
+    python_masters_path = '/home/johza721/masters_2021/python_masters/'
+import sys
+sys.path.insert(0, python_masters_path)
+submit = __import__('generate_submit')
+
+sim_name = ''  # base simulation name
+folder = ''  # in project folder root
+box_aspect = 10  # Lprl / Lprp
+cell_aspect = 2  # dx / dx_prp
+Nx_init = 64     # initial x resolution
+nodes = 1        # number of nodes
+n_cpus = 40*nodes       
+exp_rate = 0.5
+init_norm_fluc = 0.2
+beta = 0.2
+
+submit.generate_slurm(sim_name, folder, box_aspect, cell_aspect, Nx_init, n_cpus,
+                      exp_rate, init_norm_fluc, beta)
