c05cbf1d7bcab5c13ca4fc5ab92392420a38e953
Zade <johza721@student.otago.ac.nz>
2021-09-29 12:15:54 +1300

    edit2

--- run_diagnostics.py
+++ run_diagnostics.py
@@ -104,7 +104,7 @@ def run_loop(output_dir, athinput_path, dict_name='data_dump', steps=10, do_spec
         diag.save_dict(S, output_dir, dict_name)
 
     a_step = 1 if (1 + expansion_rate*(max_n-1)*dt > 2) else 0.1
-    if expansion_rate != 0:
+    if expansion_rate == 0:
         spec_step = 1
     else:
         spec_step = int(a_step / (expansion_rate*dt))  # eg if delta_a = 1, adot=0.5, dt=0.2 then spec_step = 10
